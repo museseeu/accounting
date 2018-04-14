d875335d25d20e8a1b2f6fadf6fe583931072cdc
muse <museseeu@gmail.com>
2018-04-14 14:41:09 +0800

    basic calculation good

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <FamilyList/>
+  </div>
+</template>
+
+<script>
+import FamilyList from './components/FamilyList';
+
+export default {
+  name: 'App',
+  components: {
+    FamilyList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
